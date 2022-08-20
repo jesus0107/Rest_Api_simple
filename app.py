b1900d28b3fe416b42a8b535950ffbdfab787cd0
Jesus <bep010795@gmail.com>
2022-08-19 22:22:18 -0500

    Obteniendo productos

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+from products import products
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return("Hola")
+
+@app.route("/products")
+def get_products():
+    return jsonify({"products": products, "message": "Products list"})
+
+@app.route("/products/<string:product_name>")
+def get_product(product_name):
+    print(product_name)
+    product_found = [product for product in products if product['name'] == product_name]
+    return jsonify({"product": product_found[0]})
 
 if __name__ == "__main__":
     app.run()
