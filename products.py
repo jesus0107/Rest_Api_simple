408c9423ef162bc4e9d9b964fdee4e0c9939583d
Jesus <bep010795@gmail.com>
2022-08-19 22:01:42 -0500

    Primer commit, instalacion de flask

---
+++ products.py
@@ -0,0 +1,17 @@
+products = [
+    {
+        "name": "laptop",
+        "price": 300,
+        "quantity": 5
+    },
+    {
+        "name": "keyboard",
+        "price": 100,
+        "quantity": 3
+    },
+    {
+        "name": "mouse",
+        "price": 80,
+        "quantity": 8
+    },
+]
