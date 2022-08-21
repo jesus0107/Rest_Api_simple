b01517235d5c28ce5ed7f540bc41449283b1f01a
Jesus <bep010795@gmail.com>
2022-08-20 19:02:32 -0500

    Add products

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from products import products
 
 
@@ -13,7 +13,19 @@ def get_products():
 def get_product(product_name):
     print(product_name)
     product_found = [product for product in products if product['name'] == product_name]
-    return jsonify({"product": product_found[0]})
+    if len(product_found) > 0:
+        return jsonify({"product": product_found[0]})
+    return jsonify({"message": "Product not found"})
+
+@app.route("/products/create", methods=['POST'])
+def add_product():
+    new_product = {
+        "name": request.json['name'],
+        "price": request.json['price'],
+        "quantity": request.json['quantity']
+    }
+    products.append(new_product)
+    return jsonify({"message": "Product added succesfully", "products":products})
 
 if __name__ == "__main__":
     app.run()
